2b9ab94d9af93d5340732696060fcc744c3f6c47
Adam Belak <adam_belak@epam.com>
2015-12-08 13:16:13 +0100

    Switch to ConfigurableApplicationContext

--- src/main/java/com/epam/training/springcore/App.java
+++ src/main/java/com/epam/training/springcore/App.java
@@ -1,6 +1,6 @@
 package com.epam.training.springcore;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epam.training.springcore.domain.Card;
@@ -11,11 +11,11 @@ import com.epam.training.springcore.domain.Card;
  */
 public class App {
     public static void main( String[] args ) {
-    	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-    	
-    	Card card = context.getBean("myCard", Card.class);
-    	
-        System.out.println(card);
-        
+    	try (ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+        	
+        	Card card = context.getBean("myCard", Card.class);
+        	
+            System.out.println(card);    		
+    	}
     }
 }
